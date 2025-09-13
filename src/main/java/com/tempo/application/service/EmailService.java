8fa6df96e9c8db36fabff915db2eebc0057d553e
GuillardPierre <pguillard95@gmail.com>
2025-09-13 10:29:16 +0200

    feat: :sparkles: Add email system with JavaMailSender and SimpleMailMessage to reset user password

--- src/main/java/com/tempo/application/service/EmailService.java
+++ src/main/java/com/tempo/application/service/EmailService.java
@@ -12,16 +12,28 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    @Value("${MAIL_FROM:Tempo <no-reply@tempo.app>}")
+    @Value("${MAIL_FROM:Tempo <no-reply@tempo-app.dev>}")
     private String mailFrom;
 
     public void sendPasswordResetCode(String toEmail, String code, int ttlMinutes) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
         message.setFrom(mailFrom);
-        message.setSubject("Votre code de réinitialisation");
-        message.setText("Voici votre code: " + code + "\nIl expire dans " + ttlMinutes
-                + " minutes.\nSi vous n'êtes pas à l'origine de cette demande, ignorez cet email.");
+        message.setSubject("🔐 Code de réinitialisation - Tempo");
+        message.setText(
+                "Bonjour,\n\n" +
+                        "Vous avez demandé la réinitialisation de votre mot de passe sur Tempo.\n\n" +
+                        "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n" +
+                        "   VOTRE CODE DE RÉINITIALISATION : " + code + "\n" +
+                        "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n" +
+                        "⏰ Ce code expire dans " + ttlMinutes + " minutes.\n\n" +
+                        "Pour des raisons de sécurité :\n" +
+                        "• Utilisez-le uniquement sur l'application Tempo\n" +
+                        "• Si vous n'êtes pas à l'origine de cette demande, ignorez cet email\n\n" +
+                        "Besoin d'aide ? Contactez notre support à cette adresse : pierre.guillard.dev@gmail.com\n\n" +
+                        "Cordialement,\n" +
+                        "L'équipe Tempo\n" +
+                        "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
         mailSender.send(message);
     }
 }
